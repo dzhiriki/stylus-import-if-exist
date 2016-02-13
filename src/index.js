2f691bb8f7c67b1427d92a9031b19a9789dfb4a9
Aleksandr Petrov <dzhiriki@gmail.com>
2016-02-13 16:54:53 +0300

    render block node instead root

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import path from 'path';
 import glob from 'glob';
 
 export default () => (style) => {
-  style.define('import', (file) => {
-    const res = new nodes.Root();
+  style.define('import', function (file) {
+    const result = new nodes.Block(this.currentBlock);
     const dirname = path.dirname(file.filename);
     const wildcard = path.join(dirname, file.val).replace(/(\.styl)?$/, '.styl');
     const files = glob.sync(wildcard);
 
     files.forEach((file) => {
-      res.push(
+      result.push(
         new nodes.Import(
           new nodes.String(file)
         )
       );
     });
 
-    return res;
+    return result;
   })
 }
